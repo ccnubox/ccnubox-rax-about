672748f64a14bd79dbe180449ffeeb632cac4fd4
Cruyun <1045671796@qq.com>
2018-08-28 21:28:52 +0800

    feat: about

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import {createElement, Component} from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 import styles from './App.css';
+import Image from 'rax-image';
+import Link from 'rax-link';
+import Button from 'rax-button';
 
 class App extends Component {
   render() {
     return (
       <View style={styles.app}>
-        <View style={styles.appHeader}>
-          <Text style={styles.appBanner}>Welcome to Rax</Text>
-        </View>
-        <Text style={styles.appIntro}>
-          To get started, edit src/App.js and save to reload.
-        </Text>
+        <Image
+            style={styles.icon}
+            source={require("./assets/icon.png")}
+            resizeMode="cover"
+          />
+          <Text style={[styles.fontSize, styles.purple]}>华师匣子 1.0</Text>
+          <Text style={[styles.fontSize, styles.black]}>木犀团队 出品</Text>
+          <Button style={[styles.button]}>
+            <Link href="http://www.muxixyz.com" style={[styles.white, styles.fontSize]}>访问官网</Link>
+          </Button>
       </View>
     );
   }
