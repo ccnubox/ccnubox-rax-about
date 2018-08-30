1f82f1a542655bd66a500ce4614669d0d488bc10
zxc0328 <120797887@qq.com>
2018-08-30 08:00:24 +0800

    feat: v2.0.0-beta1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import {createElement, Component} from 'rax';
-import View from 'rax-view';
-import Text from 'rax-text';
-import styles from './App.css';
-import Image from 'rax-image';
-import Link from 'rax-link';
-import Button from 'rax-button';
+import { createElement, Component, setNativeProps } from "rax";
+import View from "rax-view";
+import Text from "rax-text";
+import styles from "./App.css";
+import Image from "rax-image";
+import Link from "rax-link";
+import Button from "rax-button";
+const native = require("@weex-module/test");
 
 class App extends Component {
   render() {
     return (
       <View style={styles.app}>
         <Image
-            style={styles.icon}
-            source={require("./assets/icon.png")}
-            resizeMode="cover"
-          />
-          <Text style={[styles.fontSize, styles.purple]}>华师匣子 1.0</Text>
-          <Text style={[styles.fontSize, styles.black]}>木犀团队 出品</Text>
-          <Button style={[styles.button]}>
-            <Link href="http://www.muxixyz.com" style={[styles.white, styles.fontSize]}>访问官网</Link>
-          </Button>
+          style={styles.icon}
+          source={require("./assets/icon.png")}
+          resizeMode="cover"
+        />
+        <Text style={[styles.fontSize, styles.purple]}>华师匣子 2.0</Text>
+        <Text style={[styles.fontSize, styles.black]}>木犀团队 出品</Text>
+        <Button
+          style={[styles.button]}
+          onPress={() => {
+            native.push("http://www.muxixyz.com");
+          }}
+        >
+          <Text style={[styles.white, styles.fontSize]}>访问官网</Text>
+        </Button>
+        <Button
+          style={[styles.button, styles.button_join]}
+          onPress={() => {
+            native.joinQQGroup()
+          }}
+        >
+          <Text style={[styles.white, styles.fontSize]}>加入华师匣子交流群</Text>
+        </Button>
       </View>
     );
   }
